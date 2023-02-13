e6852039f67958475095a68b89367ec494541b61
graef <simonjn45@gmail.com>
2023-02-13 22:20:32 +0100

    Added more to define.h

--- lib/src/defines.h
+++ lib/src/defines.h
@@ -1,16 +1,37 @@
 #pragma once
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
-#define PLATFORM_WINDOWS 1
+#if defined(__clang__) || defined(__gcc__)
+#define STATIC_ASSERT _Static_assert
+#else
+#define STATIC_ASSERT static_assert
+#endif
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) 
+#define KPLATFORM_WINDOWS 1
 #ifndef _WIN64
 #error "64-bit is required on Windows!"
 #endif
 #elif defined(__linux__) || defined(__gnu_linux__)
-#define PLATFORM_LINUX 1
+#define KPLATFORM_LINUX 1
+#if defined(__ANDROID__)
+#define KPLATFORM_ANDROID 1
+#endif
 #elif defined(__unix__)
-#define PLATFORM_UNIX 1
-#elif defined(__POSIX_VERSION)
-#define PLATFORM_POSIX 1
+#define KPLATFORM_UNIX 1
+#elif defined(_POSIX_VERSION)
+#define KPLATFORM_POSIX 1
+#elif __APPLE__
+#define KPLATFORM_APPLE 1
+#include <TargetConditionals.h>
+#if TARGET_IPHONE_SIMULATOR
+#define KPLATFORM_IOS 1
+#define KPLATFORM_IOS_SIMULATOR 1
+#elif TARGET_OS_IPHONE
+#define KPLATFORM_IOS 1
+#elif TARGET_OS_MAC
+#else
+#error "Unknown Apple platform"
+#endif
 #else
 #error "Unknown platform!"
 #endif
@@ -19,7 +40,7 @@
 #ifdef _MSC_VER
 #define API __declspec(dllexport)
 #else
-#define API __attributre__((visibility("defaulk")))
+#define API __attribute__((visibility("default")))
 #endif
 #else
 #ifdef _MSC_VER
